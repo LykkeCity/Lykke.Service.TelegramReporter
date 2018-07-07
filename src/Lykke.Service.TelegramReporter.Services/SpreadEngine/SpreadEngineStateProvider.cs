6a1ef3b30e4a74824778e295d69800a93f291ce7
Taras Banakh <taras.banakh@lykke.com>
2018-07-07 14:11:50 +0300

    Adding extra data for balances.

--- src/Lykke.Service.TelegramReporter.Services/SpreadEngine/SpreadEngineStateProvider.cs
+++ src/Lykke.Service.TelegramReporter.Services/SpreadEngine/SpreadEngineStateProvider.cs
@@ -44,53 +44,83 @@ namespace Lykke.Service.TelegramReporter.Services.SpreadEngine
 
         public async Task<string> GetStateMessageAsync(int instanceId)
         {
-            var assetPairs = await _assetsService.AssetPairGetAllAsync();
+            //var assetPairs = await _assetsService.AssetPairGetAllAsync();
 
-            var taskTraders = _spreadEngineInstanceManager[instanceId]
-                .Traders.GetAsync(Array.Empty<string>());
+            const string baseAssetId = "BTC";
+            var assetPairs = new Dictionary<string, Task<InventoryModel>>
+            {
+                {"BTCUSD", null},
+                {"BTCCHF", null},
+                {"BTCJPY", null},
+                {"BTCEUR", null},
+                {"BTCGBP", null}
+            };
 
             var balancesTask = _spreadEngineInstanceManager[instanceId]
                 .Balances.GetAsync();
 
-            await Task.WhenAll(taskTraders, balancesTask);
+            var taskTraders = _spreadEngineInstanceManager[instanceId]
+                .Traders.GetAsync(Array.Empty<string>());
+
+            foreach (var assetPairId in assetPairs.Keys.ToList())
+            {
+                assetPairs[assetPairId] = _spreadEngineInstanceManager[instanceId]
+                    .Traders.GetInventoryAsync(assetPairId);
+            }
+
+            var tasks = assetPairs.Values.Cast<Task>().ToList();
+            tasks.Add(balancesTask);
+            tasks.Add(taskTraders);
+
+            await Task.WhenAll(tasks);
 
             var traders = taskTraders.Result;
 
-            var assetPairsInTraders = traders.Select(x => x.AssetPairId);
-            var assetPairsForBalances = assetPairs
-                .Where(x => assetPairsInTraders.Contains(x.Id))
-                .SelectMany(x => new[] {x.BaseAssetId, x.QuotingAssetId})
-                .Distinct()
-                .ToHashSet();
-            var balances = balancesTask.Result.Where(x => assetPairsForBalances.Contains(x.AssetId)).ToArray();
+            //var assetPairsInTraders = traders.Select(x => x.AssetPairId);
+            //var assetPairsForBalances = assetPairs
+            //    .Where(x => assetPairsInTraders.Contains(x.Id))
+            //    .SelectMany(x => new[] {x.BaseAssetId, x.QuotingAssetId})
+            //    .Distinct()
+            //    .ToHashSet();
 
-            return GetStateMessage(traders, balances);
+            var balances = balancesTask.Result;
+
+            var tradersInventories = tradersInventoriesTasks.Select(x => x.Result).ToArray();
+
+
+
+            return GetStateMessage(traders, balances, tradersInventories);
         }
 
-        private string GetStateMessage(IReadOnlyList<TraderModel> traders, IReadOnlyList<BalanceModel> balances)
+        private string GetStateMessage(IReadOnlyList<TraderModel> traders, IReadOnlyList<BalanceModel> balances, IReadOnlyList<InventoryModel> tradersInventories)
         {
             var state = new StringBuilder();
 
             state.Append($"======= {DateTime.UtcNow:yyyy/MM/dd HH:mm:ss} =======\r\n\r\n");
             state.Append($"Spread Engine State:\r\n\r\n");
 
-            foreach (var trader in traders)
+            for (var i = 0; i < traders.Count; i++)
             {
+                var trader = traders[i];
+                var traderInventory = tradersInventories[i];
+
                 state.Append(
-                    $"{trader.AssetPairId} inv: <abs inventory>; " +
+                    $"{trader.AssetPairId} inv: {traderInventory.Absolute:0.000}; " +
                     $"({trader.SellVolumeCoefficient:0.000}/{trader.BuyVolumeCoefficient:0.000}); " +
-                    $"daily: <daily sell volume>/<daily buy volume>; " +
+                    //$"daily: <daily sell volume>/<daily buy volume>; " +
                     $"PL: {trader.PnL:0.000}\r\n"
                 );
             }
 
             state.Append("\r\n");
-            state.Append("Balances\r\n");
+            state.Append("Balances:\r\n");
+
+
 
             foreach (var balance in balances)
             {
                 state.Append(
-                    $"{balance.AssetId} {balance.Amount:0.000}\r\n"
+                    $"{balance.AssetId} {balance.Amount:0.000} {:0.000} {:0.000}\r\n"
                 );
             }
 
