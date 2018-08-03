7729ee5530da1227169840c29dc849bc7a29ea78
Taras Banakh <taras.banakh@lykke.com>
2018-08-03 17:49:33 +0300

    Changed message.

---
+++ src/Lykke.Service.TelegramReporter.Services/NettingEngine/Data/TotalRowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Lykke.Service.TelegramReporter.Services.NettingEngine.Data
+{
+    public class TotalRowViewModel
+    {
+        public IReadOnlyList<ExchangeTotalViewModel> Exchanges { get; set; }
+    }
+}
