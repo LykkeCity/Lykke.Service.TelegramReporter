5faff46c0b229195cd277a37c7881285fe65e198
Taras Banakh <taras.banakh@lykke.com>
2018-08-26 23:45:18 +0300

    Small fix.

--- client/Lykke.Service.TelegramReporter.Client/Api/ITelegramReporterApi.cs
+++ client/Lykke.Service.TelegramReporter.Client/Api/ITelegramReporterApi.cs
@@ -9,40 +9,40 @@ namespace Lykke.Service.TelegramReporter.Client.Api
     [PublicAPI]
     public interface ITelegramReporterApi
     {
-        [Get("/api/v1/chatpublishercontroller/nechatpublishersettings")]
+        [Get("/api/v1/chatpublisher/nechatpublishersettings")]
         Task<IReadOnlyList<ChatPublisherSettingsDto>> GetNeChatPublisherSettingsAsync();
-        [Get("/api/v1/chatpublishercontroller/balancechatpublishersettings")]
+        [Get("/api/v1/chatpublisher/balancechatpublishersettings")]
         Task<IReadOnlyList<ChatPublisherSettingsDto>> GetBalanceChatPublisherSettingsAsync();
-        [Get("/api/v1/chatpublishercontroller/externalbalancechatpublishersettings")]
+        [Get("/api/v1/chatpublisher/externalbalancechatpublishersettings")]
         Task<IReadOnlyList<ChatPublisherSettingsDto>> GetExternalBalanceChatPublisherSettingsAsync();
 
-        [Post("/api/v1/chatpublishercontroller/nechatpublishersettings")]
+        [Post("/api/v1/chatpublisher/nechatpublishersettings")]
         Task AddNeChatPublisherSettingsAsync(ChatPublisherSettingsPost chatPublisher);
-        [Post("/api/v1/chatpublishercontroller/balancechatpublishersettings")]
+        [Post("/api/v1/chatpublisher/balancechatpublishersettings")]
         Task AddBalanceChatPublisherSettingsAsync(ChatPublisherSettingsPost chatPublisher);
-        [Post("/api/v1/chatpublishercontroller/externalbalancechatpublishersettings")]
+        [Post("/api/v1/chatpublisher/externalbalancechatpublishersettings")]
         Task AddExternalBalanceChatPublisherSettingsAsync(ChatPublisherSettingsPost chatPublisher);
 
-        [Delete("/api/v1/chatpublishercontroller/nechatpublishersettings")]
+        [Delete("/api/v1/chatpublisher/nechatpublishersettings")]
         Task RemoveNeChatPublisherSettingsAsync(string chatPublisherSettingsId);
-        [Delete("/api/v1/chatpublishercontroller/balancechatpublishersettings")]
+        [Delete("/api/v1/chatpublisher/balancechatpublishersettings")]
         Task RemoveBalanceChatPublisherSettingsAsync(string chatPublisherSettingsId);
-        [Delete("/api/v1/chatpublishercontroller/externalbalancechatpublishersettings")]
+        [Delete("/api/v1/chatpublisher/externalbalancechatpublishersettings")]
         Task RemoveExternalBalanceChatPublisherSettingsAsync(string chatPublisherSettingsId);
 
-        [Get("/api/v1/chatpublishercontroller/balanceswarnings")]
+        [Get("/api/v1/chatpublisher/balanceswarnings")]
         Task<IReadOnlyList<BalanceWarningDto>> GetBalancesWarningsAsync();
-        [Get("/api/v1/chatpublishercontroller/externalbalanceswarnings")]
+        [Get("/api/v1/chatpublisher/externalbalanceswarnings")]
         Task<IReadOnlyList<ExternalBalanceWarningDto>> GetExternalBalancesWarningsAsync();
 
-        [Post("/api/v1/chatpublishercontroller/balancewarning")]
+        [Post("/api/v1/chatpublisher/balancewarning")]
         Task AddBalanceWarningAsync(BalanceWarningPost balanceWarning);
-        [Post("/api/v1/chatpublishercontroller/externalbalancewarning")]
+        [Post("/api/v1/chatpublisher/externalbalancewarning")]
         Task AddExternalBalanceWarningAsync(ExternalBalanceWarningPost balanceWarning);
 
-        [Delete("/api/v1/chatpublishercontroller/balancewarning")]
+        [Delete("/api/v1/chatpublisher/balancewarning")]
         Task RemoveBalanceWarningAsync(string clientId, string assetId);
-        [Delete("/api/v1/chatpublishercontroller/externalbalancewarning")]
+        [Delete("/api/v1/chatpublisher/externalbalancewarning")]
         Task RemoveExternalBalanceWarningAsync(string exchange, string assetId);
     }
 }
