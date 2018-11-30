7a4b5f10161fa14bda900008c2dcc9d2e3e98716
Alexey Zelenin <alexey@zelenin.org>
2018-11-30 16:34:00 +0300

    Improved message format for MM AD

--- src/Lykke.Service.TelegramReporter.Services/MarketMakerArbitrages/MarketMakerArbitragesWarningProvider.cs
+++ src/Lykke.Service.TelegramReporter.Services/MarketMakerArbitrages/MarketMakerArbitragesWarningProvider.cs
@@ -11,32 +11,30 @@ namespace Lykke.Service.TelegramReporter.Services.MarketMakerArbitrages
         public Task<string> GetWarningMessageAsync(IMarketMakersArbitragesWarning mmArbitrageses)
         {
             var sb = new StringBuilder();
-            var rn = Environment.NewLine;
 
-            sb.AppendLine($"======= {DateTime.UtcNow:yyyy/MM/dd HH:mm:ss} ======={rn}{rn}" +
-                          $"Market Maker Arbitrages Warning:{rn}");
-
-            sb.AppendLine($"Arbitrages count: {mmArbitrageses.ArbitragesCount}{rn}");
+            sb.AppendLine($"======= {DateTime.UtcNow:yyyy/MM/dd HH:mm:ss} =======");
+            sb.AppendLine("Market Maker Arbitrages Warning:");
+            sb.AppendLine($"Total: {mmArbitrageses.ArbitragesCount}");
 
             if (mmArbitrageses.BiggestSpread.HasValue)
             {
-                sb.AppendLine($"Biggest spread: {mmArbitrageses.BiggestSpread:0.##}%");
-                sb.AppendLine($"{ mmArbitrageses.BiggestSpreadRow}{rn}");
+                sb.AppendLine($"by Spread: {mmArbitrageses.BiggestSpread:0.##}%");
+                sb.AppendLine($"{ mmArbitrageses.BiggestSpreadRow}");
             }
 
             if (mmArbitrageses.BiggestPnlInUsd.HasValue)
             {
-                sb.AppendLine($"Biggest PnL: ${mmArbitrageses.BiggestPnlInUsd:0}");
-                sb.AppendLine($"{mmArbitrageses.BiggestPnlInUsdRow}{rn}");
+                sb.AppendLine($"by PnL: ${mmArbitrageses.BiggestPnlInUsd:0}");
+                sb.AppendLine($"{mmArbitrageses.BiggestPnlInUsdRow}");
             }
 
             if (mmArbitrageses.BiggestVolumeInUsd.HasValue)
             {
-                sb.AppendLine($"Biggest volume: ${mmArbitrageses.BiggestVolumeInUsd:0}");
-                sb.AppendLine($"{mmArbitrageses.BiggestVolumeInUsdRow}{rn}");
+                sb.AppendLine($"by Volume: ${mmArbitrageses.BiggestVolumeInUsd:0}");
+                sb.AppendLine($"{mmArbitrageses.BiggestVolumeInUsdRow}");
             }
 
-            sb.AppendLine($"Most frequent asset pairs:{rn}{mmArbitrageses.MostFrequentAssetPairs}");
+            sb.AppendLine($"Most frequent asset pairs: {mmArbitrageses.MostFrequentAssetPairs}");
 
             return Task.FromResult(ChatMessageHelper.CheckSizeAndCutMessageIfNeeded(sb.ToString()));
         }
