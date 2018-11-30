818b614a27971f507e3907c98bc8538ac4b82d4a
Alexey Zelenin <alexey@zelenin.org>
2018-11-30 17:44:29 +0300

    Renamed variable

--- src/Lykke.Service.TelegramReporter.Services/MarketMakerArbitrages/MarketMakerArbitragesWarningProvider.cs
+++ src/Lykke.Service.TelegramReporter.Services/MarketMakerArbitrages/MarketMakerArbitragesWarningProvider.cs
@@ -8,7 +8,7 @@ namespace Lykke.Service.TelegramReporter.Services.MarketMakerArbitrages
 {
     public class MarketMakerArbitragesWarningProvider : IMarketMakerArbitragesWarningProvider
     {
-        public Task<string> GetWarningMessageAsync(IMarketMakersArbitragesWarning mmArbitrageses)
+        public Task<string> GetWarningMessageAsync(IMarketMakersArbitragesWarning mmArbitrages)
         {
             var sb = new StringBuilder();
 
@@ -16,29 +16,29 @@ namespace Lykke.Service.TelegramReporter.Services.MarketMakerArbitrages
             sb.Append(Environment.NewLine);
             sb.AppendLine("Market Maker Arbitrages Warning");
             sb.Append(Environment.NewLine);
-            sb.AppendLine($"Total: {mmArbitrageses.ArbitragesCount}");
+            sb.AppendLine($"Total: {mmArbitrages.ArbitragesCount}");
 
-            if (mmArbitrageses.BiggestSpread.HasValue)
+            if (mmArbitrages.BiggestSpread.HasValue)
             {
-                sb.AppendLine($"by Spread: {mmArbitrageses.BiggestSpread:0.##}%");
-                sb.AppendLine($"{ mmArbitrageses.BiggestSpreadRow}");
+                sb.AppendLine($"by Spread: {mmArbitrages.BiggestSpread:0.##}%");
+                sb.AppendLine($"{ mmArbitrages.BiggestSpreadRow}");
             }
 
-            if (mmArbitrageses.BiggestPnlInUsd.HasValue)
+            if (mmArbitrages.BiggestPnlInUsd.HasValue)
             {
-                sb.AppendLine($"by PnL: ${mmArbitrageses.BiggestPnlInUsd:0}");
-                sb.AppendLine($"{mmArbitrageses.BiggestPnlInUsdRow}");
+                sb.AppendLine($"by PnL: ${mmArbitrages.BiggestPnlInUsd:0}");
+                sb.AppendLine($"{mmArbitrages.BiggestPnlInUsdRow}");
             }
 
-            if (mmArbitrageses.BiggestVolumeInUsd.HasValue)
+            if (mmArbitrages.BiggestVolumeInUsd.HasValue)
             {
-                sb.AppendLine($"by Volume: ${mmArbitrageses.BiggestVolumeInUsd:0}");
-                sb.AppendLine($"{mmArbitrageses.BiggestVolumeInUsdRow}");
+                sb.AppendLine($"by Volume: ${mmArbitrages.BiggestVolumeInUsd:0}");
+                sb.AppendLine($"{mmArbitrages.BiggestVolumeInUsdRow}");
             }
 
             sb.Append(Environment.NewLine);
             sb.AppendLine($"Most used asset pairs:");
-            sb.AppendLine($"{mmArbitrageses.MostFrequentAssetPairs}");
+            sb.AppendLine($"{mmArbitrages.MostFrequentAssetPairs}");
 
             return Task.FromResult(ChatMessageHelper.CheckSizeAndCutMessageIfNeeded(sb.ToString()));
         }
